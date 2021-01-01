c71e6f93811c11c0dc5cd673a3b5a46922f56bac
SamHarron <harronsam@gmail.com>
2021-01-01 15:51:46 -0500

    started building user select method in cli.rb

--- lib/bored_solo_cli/cli.rb
+++ lib/bored_solo_cli/cli.rb
@@ -22,7 +22,19 @@ class CLI
         puts "Ok #{name}, let's find you something to do!"
         puts "* Type 'y' to see activities"
         puts "* Type 'exit' if you are no longer bored"
+        select
     end
 
+    def select
+        choice = input
 
+        if choice == 'y'
+            # activity list
+        elsif choice == 'exit'
+            #goodbye message
+        else
+            puts 'invalid'
+            #invalid
+        end
+    end
 end
