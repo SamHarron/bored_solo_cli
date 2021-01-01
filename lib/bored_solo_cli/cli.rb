d1f227d9bc20ca98614a82f5fdbcaf662ea298ec
SamHarron <harronsam@gmail.com>
2021-01-01 13:45:50 -0500

    added a CLI class and intro method to cli.rb

---
+++ lib/bored_solo_cli/cli.rb
@@ -0,0 +1,17 @@
+# communication between user and data
+
+class CLI 
+
+    #entry way method
+
+    def intro
+        puts ""
+        puts "Being Bored is Boring." 
+        puts "Bored Solo CLI can help you find fun activities you can do by yourself!"
+        puts ""
+        puts "Let's Begin!"
+        puts "Please type in your Name:"
+    end
+
+
+end
